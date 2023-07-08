cd86267a97aea21422511785cbf94c6ef3d8d249
maeseok <yunbum37@daum.net>
2023-07-08 18:05:40 +0900

    0708

---
+++ indexing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+col = ['col1','col2','col3']
+row = ['row1','row2','row3']
+data = [[1,2,3],[4,5,6],[7,8,9]]
+df = pd.DataFrame(data=data,index=row,columns=col)
+
+df=df.tail(2)
+print(df)

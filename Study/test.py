a829c437edecbc30ed7c103205905524cb1732d3
maeseok <yunbum37@daum.net>
2023-11-25 17:59:21 +0900

    1125

--- Study/test.py
+++ Study/test.py
@@ -1,11 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query=윤하'
-res = requests.get(url).content.decode()
-soup = BeautifulSoup(res, 'html.parser')
-
-imgs = soup.findAll('img', class_='_img')
-print(imgs)
-for img in imgs:
-	print(img.get('data-source'))
+def quad(a,b,c,x):
+    quad_term = a*x**2
+    lin_term = b*x
+    return quad_term + lin_term + c
+quad(3,5,2,6)
+print(quad_term)
+print(lin_term)
+print(a,b,c,x)
